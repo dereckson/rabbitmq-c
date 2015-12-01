6008240f4b51fcf3d0310d9d44b592034582b3bb
Alan Antonuk <alan.antonuk@gmail.com>
2015-11-30 21:59:10 -0800

    CI: use Ninja to build on linux/Mac
    Improve build performance in travis builds by using Ninja to do the
    building, which should take advantage of multiple processors when
    available.
    
    Fixes #294

--- travis.sh
+++ travis.sh
@@ -8,14 +8,14 @@ build_autotools() {
 
 build_cmake() {
   mkdir $PWD/_build && cd $PWD/_build
-  cmake .. -DCMAKE_INSTALL_PREFIX=$PWD/../_install -DCMAKE_C_FLAGS="-Werror"
+  cmake .. -GNinja -DCMAKE_INSTALL_PREFIX=$PWD/../_install -DCMAKE_C_FLAGS="-Werror"
   cmake --build . --target install
   ctest -V .
 }
 
 build_asan() {
   mkdir $PWD/_build && cd $PWD/_build
-  cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$PWD/../_install \
+  cmake .. -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$PWD/../_install \
     -DCMAKE_C_FLAGS="-Werror -fsanitize=address,undefined -O1"
   cmake --build . --target install
   ctest -V .
@@ -23,7 +23,7 @@ build_asan() {
 
 build_tsan() {
   mkdir $PWD/_build && cd $PWD/_build
-  cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$PWD/../_install \
+  cmake .. -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$PWD/../_install \
     -DCMAKE_C_FLAGS="-Werror -fsanitize=thread,undefined -O1"
   cmake --build . --target install
   ctest -V .
@@ -41,7 +41,7 @@ case $TRAVIS_OS_NAME in
 osx)
   # This prints out a long list of updated packages, which isn't useful.
   brew update > /dev/null
-  brew install popt
+  brew install popt ninja
   ;;
 esac
 
